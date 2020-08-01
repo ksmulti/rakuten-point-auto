0f4d71b6aff75eedcbc52228701f6d495e16b8ef
ksmulti <ksmulti@users.noreply.github.com>
2020-08-01 20:50:22 +0900

    For simple code
    Move some code to functions

--- rakuten_search_lib.py
+++ rakuten_search_lib.py
@@ -1,9 +1,27 @@
 import json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 def LoadSettings():
-  with open('settings.json', encoding='utf-8') as json_file:
-    settings = json.load(json_file)
-    return settings
+    with open('settings.json', encoding='utf-8') as json_file:
+        settings = json.load(json_file)
+        return settings
+
+def LoadKeywords():
+    with open('keywords.json', encoding='utf-8') as json_file:
+        keywords = json.load(json_file)
+        return keywords
+
+def WaitPageSteady(browser, wait_element_id):
+    delay = 10 # seconds
+    try:
+        myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, wait_element_id)))
+        # print("Page is ready!")
+    except TimeoutException:
+        print("Loading took too much time!")
+        exit()
 
 def Login():
-  print("Hello from a function")
+    print("Hello from a function")
--- selenium_test.py
+++ selenium_test.py
@@ -6,8 +6,12 @@ from selenium.common.exceptions import TimeoutException
 import json
 import rakuten_search_lib as RSL
 
-delay = 10 # seconds
+
 settings = RSL.LoadSettings()
+keywords = RSL.LoadKeywords()
+
+#for key in keywords['keywords']:
+#    print(key)
 
 browser = webdriver.Chrome()    #open chrome browser
 # rakuten index page
@@ -15,13 +19,7 @@ browser.get("https://websearch.rakuten.co.jp/")
 
 btn_login = browser.find_element_by_link_text("ログイン")
 btn_login.click()
-
-# rakuten login page
-try:
-    myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'loginInner_u')))
-    print("Page is ready!")
-except TimeoutException:
-    print("Loading took too much time!")
+RSL.WaitPageSteady(browser, 'loginInner_u')
 
 edit_user = browser.find_element_by_id("loginInner_u")
 edit_password = browser.find_element_by_id("loginInner_p")
@@ -30,18 +28,8 @@ edit_password.send_keys(settings["password"])
 
 btn_login = browser.find_element_by_name("submit")
 btn_login.click()
-
-# rakuten index page
-try:
-    myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'search-input')))
-    print("Page is ready!")
-except TimeoutException:
-    print("Loading took too much time!")
+RSL.WaitPageSteady(browser, 'search-input')
 
 edit_search_input = browser.find_element_by_id("search-input")
 
-with open('keywords.json', encoding='utf-8') as json_file:
-    keywords = json.load(json_file)
 
-#for key in keywords['keywords']:
-#    print(key)
